19c6fc9cf5d95d276d9abb7f0b7a076f8bc610fe
vioif <ireneaflores3@gmail.com>
2018-02-12 14:42:33 -0500

    switch working

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+var switchEle = document.querySelector('.switch');
+var bodyEle = document.querySelector('body')
+var scriptEle = document.querySelector('.status');
 
+function toggleSwitch() {
+ switchEle.classList.toggle('on');
+ switchEle.classList.toggle('off');
+ bodyEle.classList.toggle('light');
+ bodyEle.classList.toggle('dark');
+ if (scriptEle.innerText === "It's so bright in here!") {
+   scriptEle.innerText = "Hey, who turned the lights off?";
+ } else {
+   scriptEle.innerText = "It's so bright in here!";
+ }
+}
 
+switchEle.addEventListener('click', toggleSwitch);
